20923ef6b23bf9f4a9f21b625637e91d26fd2a0d
WeShop4U <weshop4u247@gmail.com>
2026-09-10 06:59:54 +0100

    Update app.config.ts uptade to v13 for ota

--- app.config.ts
+++ app.config.ts
@@ -54,7 +54,7 @@ const config: ExpoConfig = {
       }
   },
   android: {
-    versionCode: 12,
+    versionCode: 13,
     googleServicesFile: "./google-services.json",
     adaptiveIcon: {
       backgroundColor: "#0F172A",
